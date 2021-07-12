8da5e24632b3e19b0a0693e0ae5568cad75fab60
neilrush <neileorushio@gmail.com>
2021-07-12 15:04:12 -0500

    Volume from client controls progress

--- src/test/java/com/music/firebeats/FireBeatsPluginTest.java
+++ src/test/java/com/music/firebeats/FireBeatsPluginTest.java
@@ -3,9 +3,11 @@ package com.music.firebeats;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class FireBeatsPluginTest {
-	public static void main(String[] args) throws Exception {
-		ExternalPluginManager.loadBuiltin(FireBeatsPlugin.class);
+public class FireBeatsPluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(RS3MusicPlugin.class);
 		RuneLite.main(args);
 	}
 }
